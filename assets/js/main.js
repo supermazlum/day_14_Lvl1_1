cdb35a49cc476e0b489babc1f8f6c11f88fd24ec
supermazlum <123948091+supermazlum@users.noreply.github.com>
2023-02-16 13:39:25 +0100

    JS_LVL1_1

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+console.log("Hello World!");
+console.log("Anton");
+
+let firstName = ("Anton2");
+console.log(firstName)
+
+let age = (28);
+console.log(age)
+
+let job = ("Trainer");
+console.log(job);
+
+let married = (true)
+console.log("married")
+
+console.log(firstName + " ist " + age + " Jahre alt, " + "von Beruf " + job + " und ist verheiratet " + married);
